17bff2e063a40302870c2793bb2e52cd88572d61
Alice Fu <alicefu@Alices-MacBook-Pro-2.local>
2026-05-06 15:21:15 -0700

    Refactored attitude comparison

--- main.py
+++ main.py
@@ -52,18 +52,24 @@ def make_clean_make():
   subprocess.run(args=["make", "clean"], cwd=LOST_DIR)
   subprocess.run(args=["make"], cwd=LOST_DIR)
 
-''' input_path is the path to your input image. output_path is the path for the generated
-    attitude.txt. database_name is the name of your already-generated database. filter is
+''' input_path is the path to your input image. output_folder is the folder for the generated
+    attitude.txt files. database_name is the name of your already-generated database. filter is
     the Filter enum for the filter you want to use. params is an array of the remaining
-    parameters that your filter requires to be run (check image_filters.py).'''
-def run_LOST_and_filter_once(input_path: str, output_path: str, database_name: str, 
+    parameters that your filter requires to be run (check image_filters.py).
+    
+    If successful, produces two output attitude files in your output_path folder,
+    attitude.txt and filtered_attitude.txt. If not, throws a FileNotFoundError.'''
+def run_LOST_and_filter_once(input_path: str, output_folder: str, database_name: str, 
                             filter: Filter, params: list):
-  # Run LOST on input_path
+  output_path = output_folder + "/attitude.txt"
+
+  # Run LOST on input_path (pre-transformed image)
   run_lost(input_path, output_path, database_name)
 
-  # Apply filters (TODO: add customization flags to determine what is called)
-  filtered_input_path = os.path.abspath("output/filtered_image.png")
-  filtered_output_path = os.path.abspath("output/filtered_attitude.txt")
+  # Apply filters
+  filtered_input_path = output_folder + "/filtered_image.png"
+  print(filtered_input_path)
+  filtered_output_path = output_folder + "/filtered_attitude.txt"
 
   if filter == Filter.BRIGHT_OBSTRUCTION:
     print("Not yet implemented!")
@@ -75,43 +81,44 @@ def run_LOST_and_filter_once(input_path: str, output_path: str, database_name: s
                                  params[1], params[2], params[3])
   else:
     print("Invalid Filter parameter in run_pipeline_once")
+    return
 
   # Run lost on filtered_input_path
-  run_lost(filtered_input_path, filtered_output_path)
+  run_lost(filtered_input_path, filtered_output_path, database_name)
 
-  # Read attitude.txt
+def compare_attitudes(attitude_path: str, filtered_attitude_path: str):
+  # Read attitude.txt and filtered_attitude.txt
   try: 
-    with open(output_path, "r") as file: 
-      attitude = file.read()
+    with open(attitude_path, "r") as result, open(filtered_attitude_path, "r") as filtered_result:  
+      attitude = result.read()
+      filtered_attitude = filtered_result.read()
       print(attitude)
+      print(filtered_attitude)
   except FileNotFoundError:
-    print(output_path + " not found")
-
-  # Read filtered_attitude.txt
-  try: 
-    with open(filtered_output_path, "r") as file: 
-      attitude = file.read()
-      print(attitude)
-  except FileNotFoundError:
-    print(filtered_output_path + " not found")
+    print(attitude_path + " not found")
 
 # Must be run on a computer that has cloned the LOST repository
 if __name__ == "__main__":
-  # Make clean and compile LOST
+  # Make clean and compile LOST (can comment out once run once)
   make_clean_make()
 
-  # Name of image to process
-  input_path: str = os.path.abspath("input/img_7660.png")
-
-  # Name of output attitude file and generated database
-  output_path: str = os.path.abspath("output/attitude.txt")
+  # Name of output folder and generated database
+  output_folder: str = os.path.abspath("output")
   database_name: str = "my-database.dat"
 
   # Generate database
   generate_database(database_name)
 
-  # Create parameters for your filter call (index 0 is the first argument 
-  # after input_path and output_path)
+  '''This code should be in a loop: '''
+  # Name of image to process (TODO: Generate the image with LOST, and set constants to match)
+  input_path: str = os.path.abspath("input/img_7660.png")
+
+  # Parameters for your filter call (index 0 is the first argument 
+  # after img_path and output_path for the method in image_filters.py)
   params = [15, 0]
 
-  run_LOST_and_filter_once(input_path, output_path, database_name, Filter.MOTION_BLUR, params)
+  run_LOST_and_filter_once(input_path, output_folder, database_name, Filter.MOTION_BLUR, params)
+
+  output_path = output_folder + "/attitude.txt"
+  filtered_output_path = output_folder + "/filtered_attitude.txt"
+  compare_attitudes(output_path, filtered_output_path)
