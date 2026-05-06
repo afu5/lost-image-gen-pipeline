c96b5c0d68876d4fa55d76998a68a94108c457dd
Alice Fu <alicefu@Alices-MacBook-Pro-2.local>
2026-05-06 16:11:29 -0700

    Added result reporting for single result, handled errors, and factored attitude comparison

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def make_clean_make():
     parameters that your filter requires to be run (check image_filters.py).
     
     If successful, produces two output attitude files in your output_path folder,
-    attitude.txt and filtered_attitude.txt. If not, throws a FileNotFoundError.'''
+    attitude.txt and filtered_attitude.txt. If not, returns false.'''
 def run_LOST_and_filter_once(input_path: str, output_folder: str, database_name: str, 
                             filter: Filter, params: list):
   output_path = output_folder + "/attitude.txt"
@@ -68,7 +68,6 @@ def run_LOST_and_filter_once(input_path: str, output_folder: str, database_name:
 
   # Apply filters
   filtered_input_path = output_folder + "/filtered_image.png"
-  print(filtered_input_path)
   filtered_output_path = output_folder + "/filtered_attitude.txt"
 
   if filter == Filter.BRIGHT_OBSTRUCTION:
@@ -86,16 +85,54 @@ def run_LOST_and_filter_once(input_path: str, output_folder: str, database_name:
   # Run lost on filtered_input_path
   run_lost(filtered_input_path, filtered_output_path, database_name)
 
+  # Check if output files were successfully read
+  try: 
+    if load_attitude_file(output_path).get("attitude_known") == 0 or \
+       load_attitude_file(filtered_output_path).get("attitude_known") == 0:
+      print("Attitude is not found")
+      return False
+  except FileNotFoundError:
+    print("Output file not found")
+    return False
+
+  return True
+
+''' Compares attitudes given in the two input paths. Returns a dictionary
+    of percent error between attitude values for each attitude.'''
 def compare_attitudes(attitude_path: str, filtered_attitude_path: str):
   # Read attitude.txt and filtered_attitude.txt
-  try: 
-    with open(attitude_path, "r") as result, open(filtered_attitude_path, "r") as filtered_result:  
-      attitude = result.read()
-      filtered_attitude = filtered_result.read()
-      print(attitude)
-      print(filtered_attitude)
-  except FileNotFoundError:
-    print(attitude_path + " not found")
+  attitude = load_attitude_file(attitude_path)
+  filtered_attitude = load_attitude_file(filtered_attitude_path)
+  result = {}
+  for attitude_type in attitude:
+    percent_error = ((filtered_attitude[attitude_type] - attitude[attitude_type]) 
+                      / attitude[attitude_type]) * 100
+    result[attitude_type] = percent_error
+  return result
+
+''' Loads attitude file given in path, assuming it is formatted like LOST attitude output. Example:
+      attitude_known 1
+      attitude_ra 17.9868
+      attitude_de 63.4233
+      attitude_roll 12.238
+      attitude_i -0.00786361
+      attitude_j -0.5304
+      attitude_k 0.0768839
+      attitude_real -0.844218 
+    Outputs a dictionary mapping attitudes to values as floats.'''
+def load_attitude_file(path):
+    data = {}
+    with open(path, "r") as f:
+        for line in f:
+            parts = line.strip().split()
+            if len(parts) == 2:
+                key, value = parts
+                try:
+                    value = float(value)
+                except ValueError:
+                    pass
+                data[key] = value
+    return data
 
 # Must be run on a computer that has cloned the LOST repository
 if __name__ == "__main__":
@@ -106,19 +143,34 @@ if __name__ == "__main__":
   output_folder: str = os.path.abspath("output")
   database_name: str = "my-database.dat"
 
-  # Generate database
+  # Generate database (can comment out once run once)
   generate_database(database_name)
 
-  '''This code should be in a loop: '''
+  ''' This code should be in a loop: '''
   # Name of image to process (TODO: Generate the image with LOST, and set constants to match)
   input_path: str = os.path.abspath("input/img_7660.png")
 
   # Parameters for your filter call (index 0 is the first argument 
   # after img_path and output_path for the method in image_filters.py)
-  params = [15, 0]
+  params = [11, 0]
 
-  run_LOST_and_filter_once(input_path, output_folder, database_name, Filter.MOTION_BLUR, params)
+  result = run_LOST_and_filter_once(input_path, output_folder, database_name, Filter.MOTION_BLUR, params)
 
-  output_path = output_folder + "/attitude.txt"
-  filtered_output_path = output_folder + "/filtered_attitude.txt"
-  compare_attitudes(output_path, filtered_output_path)
+  if result:
+    # Compare attitudes
+    output_path = output_folder + "/attitude.txt"
+    filtered_output_path = output_folder + "/filtered_attitude.txt"
+    percent_errors = compare_attitudes(output_path, filtered_output_path)
+
+    # Check if any attitude values are more than 5 percent off
+    abs_percent_errors = [abs(x) for x in percent_errors.values()]
+    highest_percent_error = max(list(abs_percent_errors))
+    
+    if highest_percent_error > 5:
+      print("RESULT: Misidentified attitude for filtered image")
+    else:
+      print("RESULT: Successfully identified attitude for filtered image")
+    
+    print("Highest percent error: " + str(highest_percent_error))
+  else:
+    print("RESULT: Unable to identify attitude for filtered image")
